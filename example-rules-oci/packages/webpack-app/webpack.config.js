a6d42124420b5d8e1d1d127c9bd8dfb10b23e6c2
Woosang Kang <woosang.kang@bearrobotics.ai>
2023-08-08 16:03:18 -0400

    split rules_docker and rules_oci implementations

---
+++ example-rules-oci/packages/webpack-app/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    hot: true,
+    allowedHosts: 'all',
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
